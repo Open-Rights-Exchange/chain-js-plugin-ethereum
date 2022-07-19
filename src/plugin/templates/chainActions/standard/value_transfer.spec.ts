708a86c4f772c6da1882b5c26891dd5127239644
Bruno Motta <wadge.motta@gmail.com>
2022-07-19 12:07:45 -0300

    Fix tests

--- src/plugin/templates/chainActions/standard/value_transfer.spec.ts
+++ src/plugin/templates/chainActions/standard/value_transfer.spec.ts
@@ -1,26 +1,15 @@
-// ! Tests need additional configuration. This configuration needs to be done by those who understand this lib better.
-// ! But that must be the pattern of the tests.
-import { toEthereumAddress, toEthereumTxData } from '../../../helpers'
-import { erc20Abi } from '../../abis/erc20Abi'
 import { composeAction, decomposeAction } from './value_transfer'
 
 const getComposedAction = () => ({
-  to: toEthereumAddress('0x27105356f6c1ede0e92020e6225e46dc1f496b81'),
-  from: toEthereumAddress('0x0000000000000000000000000000000000000000'),
-  value: '0x00',
-  data: toEthereumTxData('0xcc872b660000000000000000000000000000000000000000000000000000000000000014'),
-  contract: {
-    abi: erc20Abi,
-    parameters: ['0x27105356f6c1ede0e92020e6225e46dc1f496b81', '20000000000000000000'], // 20 with 18 decimals of precision
-    method: 'approve',
-  },
+  from: '0x81a83975c9ed8c56c45154e42967d084ac58a17d',
+  to: '0x27105356F6C1ede0e92020e6225E46DC1F496b81',
+  value: '0xa',
 })
 
 const getDefaultArgs = () => ({
-  contractAddress: '0x27105356f6c1ede0e92020e6225e46dc1f496b81',
-  spender: '0x27105356f6c1ede0e92020e6225e46dc1f496b81',
-  precision: 18,
-  value: '20',
+  fromAccountName: '0x81a83975c9ed8c56c45154e42967d084ac58a17d',
+  toAccountName: '0x27105356F6C1ede0e92020e6225E46DC1F496b81',
+  amount: '10',
 })
 
 test('Compose ValueTransfer object', () => {
@@ -33,12 +22,13 @@ test('Decomposes ValueTransfer object', () => {
   const expAction = {
     chainActionType: 'ValueTransfer',
     args: {
-      toAccountName: toEthereumAddress('0x27105356F6C1ede0e92020e6225E46DC1F496b81'),
-      fromAccountName: toEthereumAddress('0x0000000000000000000000000000000000000000'),
+      toAccountName: '0x27105356F6C1ede0e92020e6225E46DC1F496b81',
+      fromAccountName: '0x81a83975c9ed8c56c45154e42967d084ac58a17d',
       amount: '10',
       symbol: 'wei',
     },
   }
+  // @ts-ignore
   const actAction = decomposeAction(getComposedAction())
   expect(actAction).toEqual(expAction)
 })
@@ -47,5 +37,9 @@ test('Compose and Decompose ValueTransfer', () => {
   const action = composeAction(getDefaultArgs() as any)
   const decomposed = decomposeAction(action)
 
-  expect(decomposed.args).toEqual(getDefaultArgs())
+  // ! Ideally, these should be equal, but this case is not like that.
+  // expect(decomposed.args).toEqual(getDefaultArgs())
+
+  const { symbol, ...decomposedArgs } = decomposed.args
+  expect(decomposedArgs).toEqual(getDefaultArgs())
 })
