0d1528850f754a63fd5f8342f9dc523bf1df4c24
Gaetano Mondelli <gaetano@aikon.com>
2023-05-11 08:48:19 +0100

    fix added supportsTypedDataSignature to ethereum v1

--- src/plugin/ChainEthereumV1.ts
+++ src/plugin/ChainEthereumV1.ts
@@ -296,6 +296,9 @@ class Plugin implements Interfaces.Chain {
   /** Whether the chain supports resources */
   supportsResources = false
 
+  /** Whether chain supports ability to get a publicKey from a signature */
+  supportsTypedDataSignature = true
+
   /** Verify that a 'personal message' was signed using the given key (signed with the private key for the provided public key)
    * A message differs than verifySignedWithPublicKey() because it might additional strings appended (as required by chain best-practices)
    * This differs from verifySignedWithPublicKey() because a message might include additional strings appended (as required by chain best-practices) */
