9cbbf3757ef7b5f31170f3d4c41b41e9b9acc2fa
Tray Lewin <tray@aikon.com>
2023-05-30 14:36:33 -0700

    fix static code complaint (let in switch)

--- src/plugin/ethSignMessage.ts
+++ src/plugin/ethSignMessage.ts
@@ -7,13 +7,13 @@ import {
   SignMessageOptions,
   SignMessageMethod,
   SignMessageSignTypedDataInput,
-  SignTypedDataInputModel
+  SignTypedDataInputModel,
 } from './models'
 import { personalSign, validatePersonalSignInput } from './stringSignMethods/personal-sign'
 import { signTypedData, validateSignTypedDataInput } from './stringSignMethods/sign-typed-data'
 
 export class EthereumSignMessage implements Interfaces.SignMessage {
-  constructor(message:  string, options?: SignMessageOptions) {
+  constructor(message: string, options?: SignMessageOptions) {
     this.applyOptions(options)
     this.setSignMethod()
     this.applyMessage(message)
@@ -28,20 +28,20 @@ export class EthereumSignMessage implements Interfaces.SignMessage {
 
   private _message: SignMessagePersonalSignDataInput | SignMessageSignTypedDataInput
 
-
   private applyOptions(options: SignMessageOptions) {
-    this._options = options ? options : { signMethod: SignMessageMethod.Default}
+    this._options = options || { signMethod: SignMessageMethod.Default }
   }
 
-  private applyMessage(_message: string) {
+  private applyMessage(message: string) {
+    let typedMessage: typeof SignTypedDataInputModel
     switch (this.signMethod) {
       case SignMessageMethod.EthereumSignTypedData:
-        let typedMessage = JSON.parse(_message)
-        this._message = typedMessage as typeof SignTypedDataInputModel;
-        break;
+        typedMessage = JSON.parse(message)
+        this._message = typedMessage
+        break
       case SignMessageMethod.Default:
       default:
-        this._message = { stringToSign: _message}
+        this._message = { stringToSign: message }
     }
   }
 
@@ -84,7 +84,11 @@ export class EthereumSignMessage implements Interfaces.SignMessage {
         this._isValidated = result.valid
         break
       default:
-        Errors.throwNewError(`signMethod not recognized. signMethod provided = ${this.signMethod} not in ${Object.values(SignMessageMethod)}`)
+        Errors.throwNewError(
+          `signMethod not recognized. signMethod provided = ${this.signMethod} not in ${Object.values(
+            SignMessageMethod,
+          )}`,
+        )
         break
     }
     return result
@@ -114,7 +118,11 @@ export class EthereumSignMessage implements Interfaces.SignMessage {
           result = await personalSign(privateKeys, this.message as unknown as SignMessagePersonalSignDataInput)
           break
         default:
-          Errors.throwNewError(`signMethod not recognized. signMethod provided = ${this.signMethod} not in ${Object.values(SignMessageMethod)}`)
+          Errors.throwNewError(
+            `signMethod not recognized. signMethod provided = ${this.signMethod} not in ${Object.values(
+              SignMessageMethod,
+            )}`,
+          )
           break
       }
     } catch (error) {
